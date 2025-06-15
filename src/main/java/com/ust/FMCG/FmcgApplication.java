ad2680159e22c7dfa42395158f0ac3d0886f8065
ARAVAPALLY SHIVARAMAKRISHNA <srkarvapally@gmail.com>
2025-06-15 18:51:44 +0530

    updated env

--- src/main/java/com/ust/FMCG/FmcgApplication.java
+++ src/main/java/com/ust/FMCG/FmcgApplication.java
@@ -1,6 +1,5 @@
 package com.ust.FMCG;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,8 +8,6 @@ public class FmcgApplication {
 
 	public static void main(String[] args) {
 
-//		Dotenv dotenv=Dotenv.load();
-//		System.setProperty("MONGO_URI",dotenv.get("MONGO_URI"));
 		SpringApplication.run(FmcgApplication.class, args);
 	}
 
